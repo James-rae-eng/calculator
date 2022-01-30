b3eeb19f497cb463b98fbb2860943d5c7269f80f
James Rae <jrae151515@yahoo.co.uk>
2022-01-30 11:19:30 +0000

    Initial file creation, basic buttons added on index

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+
+/*color button
+let colorToggle = 0;
+const colorButton = document.getElementById("colorButton");
+colorButton.addEventListener("click", () => {
+    greyToggle = 0;
+    if (colorToggle === 0) {
+        colorToggle = 1;
+        generateGrid();
+    }
+    else {
+        colorToggle = 0;
+        generateGrid();
+    }
+});
+*/
+
+/*clear button
+const clearButton = document.getElementById("clearButton");
+block.forEach((block) => {
+    clearButton.addEventListener("click", () => {
+        block.setAttribute("style", "background-color:white;")
+    });
+});  
+*/
+  
+
+
+
+
+
+ 
+
+
+   
+
+
